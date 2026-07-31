097dea8787835d6c96bac2bbb7cc4f020f924669
Joacim Vetterlain <joacim@vetterlain.dk>
2026-07-31 16:33:03 +0200

    feat: add sitemap integration and update related configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 3000,
   },
   site: 'https://ravudsigten.dk',
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
