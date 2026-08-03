ffc5376e75728c5943d558857e9d8ddc8a4d88a4
Joacim Vetterlain <joacim@vetterlain.dk>
2026-08-03 10:37:01 +0200

    fix: remove unnecessary build step and clean up astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { paraglide } from "@inlang/paraglide-js-adapter-vite";
 import react from '@astrojs/react';
 import { paraglideVitePlugin } from "@inlang/paraglide-js";
-import node from "@astrojs/node";
 
 import sitemap from '@astrojs/sitemap';
 
@@ -15,8 +13,11 @@ export default defineConfig({
   site: 'https://ravudsigten.dk',
   integrations: [react(), sitemap()],
   vite: {
-    plugins: [paraglideVitePlugin({ project: './project.inlang', outdir: './src/paraglide' })],
-  },
-  output: 'server',
-  adapter: node({ mode: "standalone" }),
+    plugins: [
+      paraglideVitePlugin({
+        project: './project.inlang',
+        outdir: './src/paraglide',
+      })
+    ],
+  }
 });
